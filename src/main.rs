10f6f78930b40fb20119fc46722c5bc523459d9d
XantreDev <valerik007.vs@gmail.com>
2024-08-07 10:00:52 +0200

    partially correct encoding

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-const W_TO_REG_NAME: &'static [&'static [&str; 8]; 2] = &[
-    &["al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"],
-    &["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"],
+const W_TO_REG_NAME: &'static [&'static str; 24] = &[
+    "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh", // w = 0
+    "ax", "cx", "dx", "bx", "sp", "bp", "si", "di", // w = 1
+    "bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx", // rm2registers
 ];
 
 fn main() {
@@ -13,10 +14,10 @@ fn main() {
         return;
     };
 
-    if content.len() % 2 == 1 {
-        println!("Content size must be even");
-        return;
-    }
+    // if content.len() % 2 == 1 {
+    //     println!("Content size must be even");
+    //     return;
+    // }
     let mut content_iter = content.iter();
     println!("bits 16\n");
     loop {
@@ -29,16 +30,57 @@ fn main() {
         let w = first & 0b1;
         let d = (first >> 1) & 0b1;
 
-        assert_eq!((second >> 6) & 0b11, 0b11);
+        let _mod = (second >> 6) & 0b11;
         let reg = (second >> 3) & 0b111;
         let rm = second & 0b111;
 
-        let reg_name = W_TO_REG_NAME[usize::from(w)][usize::from(reg)];
-        let rm_reg_name = W_TO_REG_NAME[usize::from(w)][usize::from(rm)];
+        match _mod {
+            0b00 | 0b01 | 0b10 => {
+                let mut displacement_number: u16 = 0;
+                let is_direct_address = _mod == 0b00 && rm == 0b110;
+                let displacement_bytes = if is_direct_address { 2 } else { _mod };
+                match displacement_bytes {
+                    2 => {
+                        displacement_number |= u16::from(*(content_iter.next().unwrap()));
+                        displacement_number |= u16::from(*(content_iter.next().unwrap())) << 8;
+                    }
+                    1 => {
+                        displacement_number |= u16::from(*(content_iter.next().unwrap()));
+                    }
+                    _ => {}
+                }
 
-        let left_reg_name = if d == 1 { reg_name } else { rm_reg_name };
-        let right_reg_name = if d == 1 { rm_reg_name } else { reg_name };
+                let registers_sum = if is_direct_address {
+                    ""
+                } else {
+                    W_TO_REG_NAME[usize::from(16 + rm)]
+                };
+                let address = &(if registers_sum.len() == 0 {
+                    format!("[{}]", displacement_number)
+                } else if displacement_number == 0 {
+                    format!("[{}]", registers_sum)
+                } else {
+                    format!("[{}+{}]", registers_sum, displacement_number)
+                });
 
-        println!("mov {}, {}", left_reg_name, right_reg_name);
+                let reg_name = &W_TO_REG_NAME[usize::from(w) * 8 + usize::from(reg)].to_string();
+                let left_reg_name = if d == 1 { reg_name } else { address };
+                let right_reg_name = if d == 1 { address } else { reg_name };
+
+                println!("mov {}, {}", left_reg_name, right_reg_name);
+            }
+            0b11 => {
+                let reg_name = W_TO_REG_NAME[usize::from(w) * 8 + usize::from(reg)];
+                let rm_reg_name = W_TO_REG_NAME[usize::from(w) * 8 + usize::from(rm)];
+
+                let left_reg_name = if d == 1 { reg_name } else { rm_reg_name };
+                let right_reg_name = if d == 1 { rm_reg_name } else { reg_name };
+
+                println!("mov {}, {}", left_reg_name, right_reg_name);
+            }
+            _ => {
+                panic!("Unknown mod");
+            }
+        }
     }
 }
