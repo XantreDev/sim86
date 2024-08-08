d9eb6a06b1302c73541096d562ba0c4cc0ad28b9
XantreDev <valerik007.vs@gmail.com>
2024-08-08 23:08:41 +0200

    immediate to reg/mem

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,39 @@ fn decode_reg(reg: u8, w: u8) -> &'static str {
     W_TO_REG_NAME[usize::from(w) * 8 + usize::from(reg)]
 }
 
-fn mov_to_register_or_memory(content_iter: &mut Iter<u8>, first: &u8, second: &u8) -> String {
+fn decode_address(content_iter: &mut Iter<u8>, _mod: u8, rm: u8) -> String {
+    let mut displacement_number: u16 = 0;
+    let is_direct_address = _mod == 0b00 && rm == 0b110;
+    let displacement_bytes = if is_direct_address { 2 } else { _mod };
+    match displacement_bytes {
+        2 => {
+            displacement_number |= u16::from(*(content_iter.next().unwrap()));
+            displacement_number |= u16::from(*(content_iter.next().unwrap())) << 8;
+        }
+        1 => {
+            displacement_number |= u16::from(*(content_iter.next().unwrap()));
+        }
+        _ => {}
+    }
+
+    let registers_sum = if is_direct_address {
+        None
+    } else {
+        Some(W_TO_REG_NAME[usize::from(16 + rm)].to_string())
+    };
+    match registers_sum {
+        None => format!("[{}]", displacement_number),
+        Some(registers_sum) => {
+            if displacement_number == 0 {
+                format!("[{}]", registers_sum)
+            } else {
+                format!("[{}+{}]", registers_sum, displacement_number)
+            }
+        }
+    }
+}
+
+fn mov_reg_to_reg(content_iter: &mut Iter<u8>, first: &u8, second: &u8) -> String {
     assert_eq!(first >> 2, 0b100010);
     let w = first & 0b1;
     let d = (first >> 1) & 0b1;
@@ -21,32 +53,7 @@ fn mov_to_register_or_memory(content_iter: &mut Iter<u8>, first: &u8, second: &u
 
     match _mod {
         0b00 | 0b01 | 0b10 => {
-            let mut displacement_number: u16 = 0;
-            let is_direct_address = _mod == 0b00 && rm == 0b110;
-            let displacement_bytes = if is_direct_address { 2 } else { _mod };
-            match displacement_bytes {
-                2 => {
-                    displacement_number |= u16::from(*(content_iter.next().unwrap()));
-                    displacement_number |= u16::from(*(content_iter.next().unwrap())) << 8;
-                }
-                1 => {
-                    displacement_number |= u16::from(*(content_iter.next().unwrap()));
-                }
-                _ => {}
-            }
-
-            let registers_sum = if is_direct_address {
-                ""
-            } else {
-                W_TO_REG_NAME[usize::from(16 + rm)]
-            };
-            let address = &(if registers_sum.len() == 0 {
-                format!("[{}]", displacement_number)
-            } else if displacement_number == 0 {
-                format!("[{}]", registers_sum)
-            } else {
-                format!("[{}+{}]", registers_sum, displacement_number)
-            });
+            let address = &decode_address(content_iter, _mod, rm);
 
             let reg_name = decode_reg(reg, w);
             let left_reg_name = if d == 1 { reg_name } else { address };
@@ -69,7 +76,27 @@ fn mov_to_register_or_memory(content_iter: &mut Iter<u8>, first: &u8, second: &u
     }
 }
 
-fn mov_to_register(content_iter: &mut Iter<u8>, first: &u8, second: &u8) -> String {
+fn mov_immediate_to_rm(content_iter: &mut Iter<u8>, first: &u8, second: &u8) -> String {
+    let w = first & 0b1;
+
+    assert!(second & 0b00111000 == 0);
+    let _mod = second >> 6 & 0b11;
+    let rm = second & 0b111;
+
+    let address = decode_address(content_iter, _mod, rm);
+
+    let mut immediate: u16 = 0;
+    immediate |= u16::from(*content_iter.next().unwrap());
+    if w == 0b1 {
+        immediate |= u16::from(*content_iter.next().unwrap()) << 8;
+    }
+
+    let word_type = if w == 0b1 { "word" } else { "byte" };
+
+    format!("mov {}, {} {}", address, word_type, immediate)
+}
+
+fn mov_immediate_to_reg(content_iter: &mut Iter<u8>, first: &u8, second: &u8) -> String {
     assert!(first >> 4 == 0b1011);
 
     let w = first >> 3 & 0b1;
@@ -110,10 +137,12 @@ fn main() {
 
         println!(
             "{}",
-            if first >> 2 == 0b100010 {
-                mov_to_register_or_memory(&mut content_iter, first, second)
+            if first >> 1 == 0b1100011 {
+                mov_immediate_to_rm(&mut content_iter, first, second)
+            } else if first >> 2 == 0b100010 {
+                mov_reg_to_reg(&mut content_iter, first, second)
             } else if first >> 4 == 0b1011 {
-                mov_to_register(&mut content_iter, first, second)
+                mov_immediate_to_reg(&mut content_iter, first, second)
             } else {
                 panic!("unknown operand")
             }
